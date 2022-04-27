52304b4cd18c619d0a6e6889f34c4efa1132fc64
SIMONETTIGUIDO1993 <104177337+SIMONETTIGUIDO1993@users.noreply.github.com>
2022-04-26 22:42:48 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+console.log("Ejercicio1.a");
+console.log("  ")
+//ingredientes para hacer una torta
+let harina: string = "2 tasas"
+let polvohornear: string = "2 cucharadas"
+let sal: string = "un cuarto de cucharada"
+let azucar: string = "1 tasa"
+
+//instrucciones para cocinar una torta
+console.log("Instrucciones para hacer torta");
+console.log("1) Antes de empezar precalienta el horno a 176 grados");
+console.log("2) Agarra el molde para tortas o dos cacerolas redondas y pon"); harina console.log(" en él");
+console.log("3) Ahora agarra la harina, el polvo de hornear y sal y revuelvelo todo en un recipiente grande. ");
+console.log("4) Seguidamente, agarra el azúcar, la manteca, los huevos, la leche y la vainilla en un recipiente separado, y mezclalo todo.");
+console.log("5) Agarra el primer recipiente con los productos secos y mezclalos con la ultima masa hecha.");
+console.log("6) Agarra la batidora y mezcla a velocidad media unos 2 o 3 minutos las dos masas hasta que queden homogénea.");
+console.log("7) Una vez tenemos la mezcla, colocala en la cacerola reparada para hornear durante 25 – 30 minutos.");
+console.log("8) Para saber si la torta esta lista, utiliza un palillo y ubicalo en el centro de la torta si sale limpio ya tienes la torta hecha!");
+console.log("Listo! a disfrutar de tu torta!");
+console.log("  ")
+
+console.log("Ejercicio1.b");
+console.log("  ")
+//instrucciones para llegar al supermercado
+console.log("Instrucciones para llegar al supermercado");
+console.log("Desde: Juan Facundo Quiroga 1051,Ushuaia, Tierra del Fuego");
+console.log("Hasta: La Anonima");
+console.log("Dirígete al nordeste por Juan Facundo Quiroga hacia Remedios de Escalada. 150 metros")
+console.log("Gira a la derecha con dirección a Raúl Muriel 130 metros");
+console.log("Gira a la derecha con dirección a Eduardo Daniel Iribarne 300 metros");
+console.log("Gira a la izquierda con dirección a Perito Francisco Moreno 190 metros");
+console.log("Gira a la derecha");
+console.log("El destino está a la derecha");
+console.log("  ")
+
+console.log("Ejercicio1.c");
+console.log("  ")
+//instrucciones para hacer un posteo en facebook
+console.log("1) Ingresar a https://www.facebook.com/...");
+console.log("2) Pensar que publicar...");
+console.log("3) Selecciona Publicar algo...");
+console.log("4) Elige la página en la que quieres hacer la publicación.");
+console.log("5) Selecciona el tipo de publicación que quieres hacer.");
+console.log("6) Puedes publicar cualquier tipo de contenido de tu página que cumpla los requisitos para publicarse mediante Creator Studio. Crea la publicación y selecciona Publicar.");
+
